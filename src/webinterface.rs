b483c2aacf294606347a1a0bd695c23e37c32ad2
PakL <webmaster@paklweb.de>
2023-09-30 18:37:32 +0200

    feat: actually implement aliases and added alias_group parameter

--- src/webinterface.rs
+++ src/webinterface.rs
@@ -32,12 +32,15 @@ struct WebhookEventQuery {
 	#[serde(rename = "type")]
 	event_type: Option<u8>,
 	form: String,
+	alias_group: String,
 	count: u16,
 }
 
 #[derive(Serialize, Deserialize)]
 struct WebhookNamedEventQuery {
 	form: String,
+	#[serde(default, skip_serializing)]
+	alias_group: String,
 	name: String,
 }
 
@@ -167,21 +170,56 @@ fn write_file_with_retry<T: Serialize>(path: &Path, data: &T) -> Option<std::io:
 	result
 }
 
+fn formid_or_alias(form: String, alias_group: String) -> String {
+	let mut form_id;
+	if alias_group.len() > 0 {
+		let mut form_from_alias = String::new();
+		if let Some(alias) = aliases::get_alias(&form) {
+			if alias_group == "*" {
+				form_from_alias = alias.form;
+			} else {
+				let groups = alias.filter_group.split(',');
+				for group in groups {
+					if group == alias_group {
+						form_from_alias = alias.form;
+						break;
+					}
+				}
+			}
+		}
+
+		form_id = prepare_formid(form_from_alias, true);
+	} else {
+		form_id = prepare_formid(form.clone(), true);
+		if form_id.is_empty() {
+			if let Some(alias) = aliases::get_alias(&form) {
+				form_id = prepare_formid(alias.form, true);
+			}
+		}
+	}
+
+	form_id
+}
+
 fn default_response_callback(query: WebhookEventQuery, skyrim_path: String, alias_form: HashMap<String, String>) -> reply::WithStatus<reply::Html<String>> {
 	if let Some(event_type) = query.event_type {
 		println!("> Received event {}", json!(query));
 		if event_type <= 5 {
+			let form = formid_or_alias(query.form, query.alias_group);
+			if form.is_empty() {
+				println!("x Invalid form");
+				return reply::with_status(reply::html("invalid form".to_string()), warp::http::StatusCode::BAD_REQUEST);
+			}
+
 			let path_buf = Path::new(&skyrim_path).join("events.ptw");
 			let path = path_buf.as_path();
 			let mut events = read_file_with_retry::<Vec<WebhookEventQuery>>(path).unwrap_or_default();
 			let new_event = WebhookEventQuery {
 				event_type: Some(event_type),
-				form: prepare_formid(query.form, true),
+				form,
+				alias_group: String::new(),
 				count: if query.count < 1 { 1 } else { query.count },
 			};
-			if new_event.form.is_empty() {
-				return reply::with_status(reply::html("invalid form".to_string()), warp::http::StatusCode::BAD_REQUEST);
-			}
 
 			let event_json = json!(new_event);
 			events.append(&mut vec![new_event]);
@@ -196,7 +234,7 @@ fn default_response_callback(query: WebhookEventQuery, skyrim_path: String, alia
 			};
 			reply::with_status(reply::html(message), status)
 		} else {
-			println!("x Invalid form");
+			println!("x Invalid event type");
 			reply::with_status(reply::html("invalid type".to_string()), warp::http::StatusCode::BAD_REQUEST)
 		}
 	} else {
@@ -265,17 +303,21 @@ fn search_response_callback(query: SearchQuery) -> reply::WithStatus<reply::Html
 
 fn named_spawns_response_callback(query: WebhookNamedEventQuery, spawn_file: &str, skyrim_path: String) -> reply::WithStatus<reply::Html<String>> {
 	println!("> Received help request {}", json!(query));
+
+	let form = formid_or_alias(query.form, query.alias_group);
+	if form.is_empty() {
+		println!("x Invalid form");
+		return reply::with_status(reply::html("invalid form".to_string()), warp::http::StatusCode::BAD_REQUEST);
+	}
 	
 	let path_buf = Path::new(&skyrim_path).join(spawn_file);
 	let path = path_buf.as_path();
 	let mut events = read_file_with_retry::<Vec<WebhookNamedEventQuery>>(path).unwrap_or_default();
 	let new_event = WebhookNamedEventQuery {
-		form: prepare_formid(query.form, true),
+		form,
+		alias_group: String::new(),
 		name: query.name
 	};
-	if new_event.form.is_empty() {
-		return reply::with_status(reply::html("invalid form".to_string()), warp::http::StatusCode::BAD_REQUEST);
-	}
 
 	let event_json = json!(new_event);
 	events.append(&mut vec![new_event]);
